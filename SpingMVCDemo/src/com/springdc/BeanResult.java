dce9f2e5e5087c96dabbc4e4f9970a575670fb64
50887355 <50887355@qq.com>
2017-09-01 16:27:59 +0800

    1

---
+++ SpingMVCDemo/src/com/springdc/BeanResult.java
@@ -0,0 +1,17 @@
+package com.springdc;
+
+import java.util.List;
+
+public class BeanResult<T> {
+	private List<T> resultlist;
+
+	public List<T> getResultlist() {
+		return resultlist;
+	}
+
+	public void setResultlist(List<T> resultlist) {
+		this.resultlist = resultlist;
+	}
+	
+	
+}
