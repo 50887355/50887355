94cdd480fc0ac55f32d4bccd048a4f2777c1e3b8
50887355 <50887355@qq.com>
2017-09-01 16:35:33 +0800

    1

--- SpingMVCDemo/src/com/springdc/AbstractBaseController.java
+++ SpingMVCDemo/src/com/springdc/AbstractBaseController.java
@@ -3,9 +3,9 @@ package com.springdc;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.JsonViewResponseBodyAdvice;
 
@@ -16,7 +16,7 @@ public abstract class AbstractBaseController<T> {
 	
 	@ResponseBody
 	@RequestMapping("/page/helloworld")
-	public BeanResult<T> query(HttpServletRequest request){
+	public BeanResult<T> query(PageParm pageparm,@RequestParam(value="searcharray", required=false) String searcharray){
 
 		return getBaseService().query();
 	}
