f17566a0fe88aa89571412d7ccf0df6a50337313
50887355 <50887355@qq.com>
2017-09-05 16:08:42 +0800

    1

---
+++ SpingMVCDemo/src/com/springmvc/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+        //@RequestMapping(method = RequestMethod.GET)
+		@RequestMapping("/helloWorld")
+		@ResponseBody
+		public String hello(ModelMap model) {
+        	System.out.println("-------------");
+            model.addAttribute("name", "Hello World!");
+            return "helloWorld";
+
+        }
+
+    }
